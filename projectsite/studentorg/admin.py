de3f8823823943cd4f6da64e28d86873f3cfdb92
CastleKen <202380191@psu.palawan.edu.phgit config --global user.name>
2025-08-21 12:43:17 +0800

    fix search fields in admin

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -6,19 +6,19 @@ from .models import College, Program, Organization, Student, OrgMember
 @admin.register(College)
 class CollegeAdmin(admin.ModelAdmin):
 	list_display = ("college_name", "created_at", "updated_at",)
-	search_fields = ("college",)
+	search_fields = ("college_name",)
 	list_filter = ("created_at",)
 	
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
 	list_display = ("prog_name", "college",)
-	search_fields = ("program", "college",)
+	search_fields = ("prog_name", "college__college_name",)
 	list_filter = ("college",)
 	
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
 	list_display = ("name", "college", "description",)
-	search_fields = ("organization_name", "description",)
+	search_fields = ("name", "description",)
 	list_filter = ("college",)
 
 @admin.register(Student)
@@ -30,7 +30,7 @@ class StudentAdmin(admin.ModelAdmin):
 class OrgMemberAdmin(admin.ModelAdmin):
 	list_display = ("student", "get_member_program", "organization",
 			"date_joined",)
-	search_fields = ("student_lastname", "student_firstname",)
+	search_fields = ("student__lastname", "student__firstname",)
 
 	def get_member_program(self, obj):
 		try:
