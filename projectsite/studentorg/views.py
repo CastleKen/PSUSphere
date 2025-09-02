5e1abecbcd4e3ceaf30dc9c76e2ad0a4a575d1ba
CastleKen <202380191@psu.palawan.edu.phgit config --global user.name>
2025-09-02 10:01:05 +0800

    added Program ListView

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from studentorg.models import Organization, OrgMember, Student, College
+from studentorg.models import Organization, OrgMember, Student, College, Program
 from studentorg.forms import OrganizationForm, OrgMemberForm, StudentForm, CollegeForm
 from django.urls import reverse_lazy
 
@@ -103,3 +103,9 @@ class CollegeDeleteView(DeleteView):
     model = College
     template_name = 'college_del.html'
     success_url = reverse_lazy('college-list')
+
+class ProgramList(ListView):
+    model = Program
+    context_object_name = 'program'
+    template_name = 'program_list.html'
+    paginate_by = 5
